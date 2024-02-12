624a02d5b3291f3f585f7f314af8ee6581a9c1d9
Alan Cleyton <alancleyton67@gmail.com>
2024-02-12 10:38:15 -0300

    test: add vitest and testing-library setup

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+import { expect } from 'vitest';
+import { type TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+declare module 'vitest' {
+  interface Assertion<T = any>
+    extends jest.Matchers<void, T>,
+      TestingLibraryMatchers<T, void> {}
+}
+
+expect.extend(matchers);
