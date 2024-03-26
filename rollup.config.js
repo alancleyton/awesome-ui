e44908404af5b59afd2296cbe4d4dbfb56e919d4
Alan Cleyton <alancleyton67@gmail.com>
2024-03-26 11:54:16 -0300

    fix(rollup): remove rollup-plugin-dts

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,14 @@ import * as fs from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+// import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import css from 'rollup-plugin-import-css';
-import dts from 'rollup-plugin-dts';
-import alias from '@rollup/plugin-alias';
-import path from 'path';
+// import dts from 'rollup-plugin-dts';
+// import alias from '@rollup/plugin-alias';
+// import path from 'path';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
@@ -48,21 +48,21 @@ export default [
       css(),
     ],
   },
-  {
-    external: externalDeps,
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [
-      alias({
-        entries: [
-          {
-            find: '@awesome-ui',
-            replacement: path.resolve('./src'),
-          },
-        ],
-      }),
-      dts(),
-      typescriptPaths(),
-    ],
-  },
+  // {
+  //   external: externalDeps,
+  //   input: 'dist/esm/types/index.d.ts',
+  //   output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+  //   plugins: [
+  //     alias({
+  //       entries: [
+  //         {
+  //           find: '@awesome-ui',
+  //           replacement: path.resolve('./src'),
+  //         },
+  //       ],
+  //     }),
+  //     dts(),
+  //     typescriptPaths(),
+  //   ],
+  // },
 ];
