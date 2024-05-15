dd2a8606a0dc5ccd528dfaec9e250851264afdfc
Alan Cleyton <alancleyton67@gmail.com>
2024-05-15 14:35:00 -0300

    fix: adjust button component types docs

--- src/components/button/button.types.ts
+++ src/components/button/button.types.ts
@@ -2,32 +2,40 @@ import type { ReactNode, ForwardedRef } from 'react';
 import type { ButtonProps as AriaButtonProps } from 'react-aria-components';
 
 type ButtonRef = ForwardedRef<HTMLButtonElement>;
+type ColorVariants =
+  | 'primary'
+  | 'secondary'
+  | 'info'
+  | 'danger'
+  | 'success'
+  | 'ghost'
+  | 'card';
 
 interface ButtonProps extends AriaButtonProps {
   children?: ReactNode;
 
   /**
-   * Applies color variants to the button.
-   * The default variant color is `primary`.
+   * Applies color variant to the button.
+   *
+   * - Available variants: `primary`, `secondary`, `info`, `danger`, `success`, `ghost`, `card`
+   *
+   * @default `primary`
    */
-  variant?:
-    | 'primary'
-    | 'secondary'
-    | 'info'
-    | 'danger'
-    | 'success'
-    | 'ghost'
-    | 'card';
+  variant?: ColorVariants;
 
   /**
    * Applies the size of the button.
-   * The default size is `md`.
+   *
+   * - Available sizes: `sm`, `md`, `lg`
+   *
+   * @default `md`
    */
   size?: 'sm' | 'md' | 'lg';
 
   /**
    * Applies 100% to the button width.
-   * The default size is `false`.
+   *
+   * @default `false`
    */
   isFull?: boolean;
 }
