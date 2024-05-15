cd12492eed12c92c899f5b08ae50612550874044
Alan Cleyton <alancleyton67@gmail.com>
2024-05-15 11:14:37 -0300

    docs: add button docs to types

--- src/components/button/button.types.ts
+++ src/components/button/button.types.ts
@@ -1,12 +1,35 @@
 import type { ReactNode, ForwardedRef } from 'react';
 import type { ButtonProps as AriaButtonProps } from 'react-aria-components';
 
-import type { ButtonVariantProps } from '@awesome-ui/components/button/button.styles';
-
 type ButtonRef = ForwardedRef<HTMLButtonElement>;
 
-interface ButtonProps extends AriaButtonProps, ButtonVariantProps {
+interface ButtonProps extends AriaButtonProps {
   children?: ReactNode;
+
+  /**
+   * Applies color variants to the button.
+   * The default variant color is `primary`.
+   */
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'info'
+    | 'danger'
+    | 'success'
+    | 'ghost'
+    | 'card';
+
+  /**
+   * Applies the size of the button.
+   * The default size is `md`.
+   */
+  size?: 'sm' | 'md' | 'lg';
+
+  /**
+   * Applies 100% to the button width.
+   * The default size is `false`.
+   */
+  isFull?: boolean;
 }
 
 export type { ButtonRef, ButtonProps };
