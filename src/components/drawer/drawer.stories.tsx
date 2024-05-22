795ff163b6695d397b91255dfb270fca4cde7b82
Alan Cleyton <alancleyton67@gmail.com>
2024-05-22 17:52:54 -0300

    docs: add storybooks docs to drawer component

--- src/components/drawer/drawer.stories.tsx
+++ src/components/drawer/drawer.stories.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import type { Meta } from '@storybook/react';
+/* eslint react-hooks/rules-of-hooks: off */
+import { useArgs } from '@storybook/store';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from '@awesome-ui/components/button';
 import { Drawer, DrawerProps } from '.';
@@ -9,177 +10,99 @@ export default {
   component: Drawer,
 } as Meta<DrawerProps>;
 
-export const DrawerTop = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+type Story = StoryObj<DrawerProps & { label?: string }>;
 
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
+const DrawerTemplate: Story = {
+  render: ({ open, placement, size, label }) => {
+    const [, setArgs] = useArgs();
+    const handleOpenDrawer = () => setArgs({ open: true });
+    const handleCloseDrawer = () => setArgs({ open: false });
 
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open top drawer</Button>
-      <Drawer placement="top" open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+    return (
+      <div>
+        <Button onPress={handleOpenDrawer}>{label}</Button>
+        <Drawer
+          open={open}
+          placement={placement}
+          size={size}
+          onClose={handleCloseDrawer}
+        >
+          <Drawer.Content>
+            <div className="a-p-5 a-h-full a-bg-white">
+              <p>drawer content</p>
+              <Button onPress={handleCloseDrawer}>close drawer</Button>
+            </div>
+          </Drawer.Content>
+        </Drawer>
+      </div>
+    );
+  },
 };
 
-export const DrawerLeft = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open left drawer</Button>
-      <Drawer open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerTop: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'top drawer',
+    placement: 'top',
+  },
 };
 
-export const DrawerRight = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open right drawer</Button>
-      <Drawer
-        placement="right"
-        open={isOpen}
-        onClose={() => toggleDrawer(false)}
-      >
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerRight: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'right drawer',
+    placement: 'right',
+  },
 };
 
-export const DrawerBottom = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open bottom drawer</Button>
-      <Drawer
-        placement="bottom"
-        open={isOpen}
-        onClose={() => toggleDrawer(false)}
-      >
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerBottom: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'bottom drawer',
+    placement: 'bottom',
+  },
 };
 
-export const DrawerSM = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open bottom drawer</Button>
-      <Drawer size="sm" open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerLeft: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'left drawer',
+    placement: 'left',
+  },
 };
 
-export const DrawerMD = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open bottom drawer</Button>
-      <Drawer size="md" open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerSM: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'small drawer',
+    placement: 'left',
+    size: 'sm',
+  },
 };
 
-export const DrawerLG = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
-
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open bottom drawer</Button>
-      <Drawer size="lg" open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerMD: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'medium drawer',
+    placement: 'left',
+    size: 'md',
+  },
 };
-export const DrawerFull = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const toggleDrawer = (param: boolean) => {
-    setIsOpen(param);
-  };
+export const DrawerLG: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'large drawer',
+    placement: 'left',
+    size: 'lg',
+  },
+};
 
-  return (
-    <div>
-      <Button onPress={() => toggleDrawer(true)}>open full drawer</Button>
-      <Drawer size="full" open={isOpen} onClose={() => toggleDrawer(false)}>
-        <Drawer.Content>
-          <div className="a-bg-white a-h-full">
-            <p>drawer content</p>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+export const DrawerFull: Story = {
+  ...DrawerTemplate,
+  args: {
+    label: 'full drawer',
+    placement: 'left',
+    size: 'full',
+  },
 };
