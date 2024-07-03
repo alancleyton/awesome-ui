b1c9095a4627d33ef32da4c617d9f118018fba18
Alan Cleyton <alancleyton67@gmail.com>
2024-07-03 16:14:21 -0300

    feat(input): exports component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from '@awesome-ui/components/button';
 export * from '@awesome-ui/components/navbar';
 export * from '@awesome-ui/components/drawer';
+export * from '@awesome-ui/components/input';
